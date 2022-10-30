26d46772d796bde5d452370b26f7f7fd41f1b24b
Nedo <balic94@gmail.com>
2022-10-30 20:18:52 +0100

    fixed 01,02,03 exerices

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    var tmpstrin = ""
+    if(num < 0) {
+        return("ERROR");
+    }
+    else {
+        for (var i = 0; i < num; i++) {
+            tmpstrin += string;
+        }
+    }
+    return tmpstrin;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let tempString = "";
+    for (var i = string.length - 1; i >= 0; i--) {
+        tempString += string[i];
+    }
+    return tempString;
 };
 
 // Do not edit below this line
