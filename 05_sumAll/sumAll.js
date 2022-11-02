e89baa4005bbf458b227cf348e2e600ce5554ee4
Nedo <balic94@gmail.com>
2022-11-02 21:42:43 +0100

    updated 05_summAll 06_leapYears 07_tempConversion

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
-
-};
+const sumAll = function(num1, num2) {
+    let tmpNum = 0;
+    let a, b, i;
+    if (((num1 < 0) || (num2 < 0)) || ((typeof num1 != 'number') || (typeof num2 != 'number'))){
+        return("ERROR");
+    }
+    if (num1 > num2) {
+        a = num2;
+        b = num1;
+    }
+    else {
+        a = num1;
+        b = num2;
+    }
+    for (i = a; i <= b; i++) {
+        tmpNum += i;
+    }
+    return tmpNum;
+  };
 
 // Do not edit below this line
 module.exports = sumAll;
